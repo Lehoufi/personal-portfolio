e07391317a5c0e7a409ec4236817d5ffd03d85bb
lehoufix 100o <lehoufi.said@gmail.com>
2023-12-24 15:05:00 +0100

    Portu

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -20,7 +20,7 @@ export default function Page({ updatepost,primaryColor }) {
     transition={{ delay: 0.175 }} className='fx  justify-center max-md:mb-[-90px] max-md:mt-[-100px]' id='home'>
       <div ref={ref} className="home mt-[100px] text-gray-900 dark:text-gray-200">
         <div  className='text-6xl max-md:text-4xl font-black text-center'>
-          <p >hello .🌍 , i'm <span style={{ color: primaryColor }} className='transition-all duration-400'>Lehoufi</span></p>
+          <p >hello .🌍 , I&apos;m <span style={{ color: primaryColor }} className='transition-all duration-400'>Lehoufi</span></p>
         </div>
         <div className="fx gap-2 text-[#5e5e63] dark:text-[#d1d1d1]  ">
         <svg viewBox="0 0 24 24" className='w-6 max-md:w-5' fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracurrentColorerCarrier" stroke-linecurrentcap="round" strokeLinejoin="round"></g><g id="SVGRepo_icurrentColoronCarrier"> <path fillRule="evenodd"  d="M12 6C9.79086 6 8 7.79086 8 10C8 12.2091 9.79086 14 12 14C14.2091 14 16 12.2091 16 10C16 7.79086 14.2091 6 12 6ZM12 8C10.8954 8 10 8.89543 10 10C10 11.1046 10.8954 12 12 12C13.1046 12 14 11.1046 14 10C14 8.89543 13.1046 8 12 8Z" fill="currentColor"></path> <path fillRule="evenodd"  d="M11.0901 22.5958C11.2059 22.6753 11.2981 22.7366 11.3626 22.7788L11.4687 22.8472C11.7933 23.0504 12.2061 23.0508 12.5307 22.8476L12.6375 22.7788C12.7019 22.7366 12.7941 22.6753 12.9099 22.5958C13.1415 22.4367 13.4685 22.2041 13.8591 21.9041C14.6386 21.3054 15.6801 20.4322 16.7247 19.3336C18.7857 17.1661 21 13.9725 21 10.1818C21 7.75381 20.0571 5.42084 18.3719 3.69728C16.6859 1.97296 14.3943 1 12 1C9.60571 1 7.31415 1.97296 5.62814 3.69728C3.94288 5.42084 3 7.75381 3 10.1818C3 13.9725 5.21434 17.1661 7.27531 19.3336C8.31993 20.4322 9.36136 21.3054 10.1409 21.9041C10.5315 22.2041 10.8585 22.4367 11.0901 22.5958ZM12 3C10.1508 3 8.37273 3.75107 7.05815 5.09552C5.74283 6.44073 5 8.26992 5 10.1818C5 13.2007 6.78566 15.9162 8.72469 17.9554C9.68007 18.9602 10.6386 19.7646 11.3591 20.3179C11.6046 20.5065 11.8215 20.6651 12 20.7918C12.1785 20.6651 12.3954 20.5065 12.6409 20.3179C13.3614 19.7646 14.3199 18.9602 15.2753 17.9554C17.2143 15.9162 19 13.2007 19 10.1818C19 8.26992 18.2572 6.44073 16.9418 5.09552C15.6273 3.75107 13.8492 3 12 3Z" fill="currentColor"></path> </g></svg>
@@ -28,7 +28,7 @@ export default function Page({ updatepost,primaryColor }) {
           <p className='text-xl font-bold max-md:text-lg  text-[#5e5e63] dark:text-[#d1d1d1]  '>Morocco</p>
         </div>
         <p className='font-bold max-w-[90%] max-md:max-w[99%] max-md:max-w-full md:max-w-[600px] text-center leading-loose text-[#5e5e63] dark:text-[#d1d1d1]  '>
-          I'm a student with a diploma in Information System Development, and my ambition lies in the realm of web design. I'm passionate about creating visually appealing and user-friendly websites, and I'm excited to explore the creative possibilities in this field 
+          I&apos;m a student with a diploma in Information System Development, and my ambition lies in the realm of web design. I&apos;m passionate about creating visually appealing and user-friendly websites, and I&apos;m excited to explore the creative possibilities in this field 
         </p>
       </div>
 
